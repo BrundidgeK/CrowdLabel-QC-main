b03f393ea98977e384982622a8e939df8f063ef5
Mina Ly-Hang <mklyhang@ncsu.edu>
2026-02-23 19:34:25 -0500

    continued changes

--- tests/test_tag_report_output.py
+++ tests/test_tag_report_output.py
@@ -1,20 +1,26 @@
 """Integration tests for tag report output as CSV"""
 
+
 from src.qcc.io.csv_adapter import read_assignments
 from src.qcc.reports.tag_reports.tag_report_output import write_to_csv
 
 class TestTagReportOutput:
 
     def setup_method(self):
-        a =1 
+        a = 1 
         # set up
+        pass
+
 
     def test():
-        csv_input = "/tests/data/min.csv"
-        csv_output = "/tests/data/"
+        csv_input = "tests/data/min.csv"
+        csv_output = "/tests/data/tag_report_output.csv"
 
         tags = read_assignments(csv_input)
-        write_to_csv(tags, csv_output)
+        report = TagReportOutput()
+        report.write_to_csv(tags, csv_output)
+
+
 
 
 
